34547dfef2bb4258d7d4a06353d9fde64a6b30c7
Anh <anh.le.nvsu@gmail.com>
2025-10-30 11:12:58 -0400

    fix logistic to add sigmoid

--- LogisticRegression.py
+++ LogisticRegression.py
@@ -4,24 +4,29 @@ from LinearRegression import normalize
 
 
 class LogisitcRegressionScratch:
-    def __init__(self, a=0.001, epsilon=0.1, epoch=1000):
+    def __init__(self, a=0.001, epsilon=0.1, epoch=1000, fit_intercept=True):
         self.a = a
         self.epsilon = epsilon
         self.epoch = epoch
-        self.weights = None
+        self.fit_intercept = fit_intercept
+
+    def add_intercept(self, X):
+        intercept = np.ones((X.shape[0], 1))
+        return np.concatenate((intercept, X), axis=1)
+
+    def get_sigmoid(self, z):
+        return 1 / (1 + np.exp(-z))
 
     def get_predicted_values(self, weights, X):
-        return weights[0] + np.dot(X, weights[1:])
+        return np.dot(X, weights)
 
-    def compute_gradient(self, X, y, weights, n):
-        predicted = self.get_predicted_values(weights=weights, X=X)
+    def compute_gradient(self, X, y, predicted, n):
         residual = predicted - y
         residual_feature = X.T * residual
         return 1 / n * np.sum(residual_feature)
 
-    def compute_err(self, X, y, weights, n):
-        predicted = self.get_predicted_values(weights=weights, X=X)
-        err_each = -y * (np.log2(predicted)) - (1 - y)(np.log2(1 - predicted))
+    def compute_err(self, y, sigmoid, n):
+        err_each = -y * (np.log2(sigmoid)) - (1 - y)(np.log2(1 - sigmoid))
         return 1 / n * np.sum(err_each)
 
     def update_weights(self, weights, gradient):
@@ -31,14 +36,17 @@ class LogisitcRegressionScratch:
         n = len(X)
         X = normalize(X)
         y = normalize(y)
-        weights = np.random.rand(len(X[0]) + 1)
+        if self.fit_intercept:
+            X = self.add_intercept(X)
+        self.weights = np.random.rand(len(X[0]) + 1)
         for _ in range(self.epoch):
-            gradients = self.compute_gradient(X, y, weights=weights, n=n)
-            new_weights = self.update_weights(weights, gradients)
-            if abs(new_weights - weights) < self.epsilon:
+            predicted = self.get_predicted_values(self.weights, X)
+            sigmoid = self.get_sigmoid(predicted)
+            gradients = self.compute_gradient(y, sigmoid, n=n)
+            new_weights = self.update_weights(self.weights, gradients)
+            if abs(new_weights - self.weights) < self.epsilon:
                 break
-            weights = new_weights
-        self.weights = weights
+            self.weights = new_weights
         return
 
     def predict(self, X_test):
